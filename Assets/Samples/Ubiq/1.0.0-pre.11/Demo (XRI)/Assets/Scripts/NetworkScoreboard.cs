5928f9e26b5c0ca552a23e236fdeae0fdf515ecc
nono <97236799+Peanut-Puff@users.noreply.github.com>
2025-03-24 02:03:47 +0000

    Update NetworkScoreboard.cs

--- Assets/Samples/Ubiq/1.0.0-pre.11/Demo (XRI)/Assets/Scripts/NetworkScoreboard.cs
+++ Assets/Samples/Ubiq/1.0.0-pre.11/Demo (XRI)/Assets/Scripts/NetworkScoreboard.cs
@@ -88,7 +88,7 @@ namespace Ubiq.Samples
 
         public void AddScore(string team, int amount)
         {
-            if (!enabled) return;
+            if (!enabled) {Debug.Log("score not enabled")};
             Debug.Log("score");
             if (team == "hider") hiderScore += amount;
             else if (team == "catcher") catcherScore += amount;
